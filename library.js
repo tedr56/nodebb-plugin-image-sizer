5671ad6dccb682cb79c840c6eb167568eff2df91
VJ T <tedr56@gmail.com>
2014-12-12 11:01:43 +0100

    Compatibility to NodeBB 0.6.0

--- library.js
+++ library.js
@@ -29,12 +29,16 @@ function getMultString(match, src, mult)
 }
 
 var ImageSizer = {
-    sizeImages: function(postData, callback) {
+    sizeImages: function(data, callback) {
+	if (!data || !data.postData || !data.postData.content) {
+	    return callback(null, data);
+	}
+
         percentRegex = /<img src="([^@]*)@([0-9]+)%(25)?"/g; // TODO: figure out why "25" gets appended on v0.5.2
         absoluteRegex = /<img src="([^@]*)@([0-9]*)x([0-9]*)"/g;
         multiplyRegex = /<img src="([^@]*)@([0-9]*\.?[0-9]*)"/g;
 
-        var tmp = postData;
+        var tmp = data.postData.content;
 
         tmp = tmp.replace(percentRegex, getPercentString);
         tmp = tmp.replace(multiplyRegex, getMultString);
